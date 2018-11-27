9fcb01c1f607c4339ca0a86f372ecc042f780d1d
Silvia N <silvia.xeraco@gmail.com>
2018-11-27 16:40:27 +0100

    entregable teoria tema 4 por terminar

---
+++ entregable_teoria/entr_t4.m
@@ -0,0 +1,62 @@
+#!/usr/bin/octave -qf
+
+#### SEPARAR UN CONJUNTO DE ENTRENAMIENTO Y OTRO DE TEST ####
+load('data/usps/tr.dat');           # training
+load('data/usps/trlabels.dat');
+load('data/usps/ts.dat');           # test
+load('data/usps/tslabels.dat');
+
+#### EXTRAER LOS DATOS DE LAS CLASES 0 A 3 ####
+posiciones_0 = find(trlabels == 0);
+data_0 = tr(posiciones_0, :);
+labels_0 = trlabels(posiciones_0, :);
+posiciones_1 = find(trlabels_1 == 1);
+data_1 = tr(posiciones_1, :);
+labels_1 = trlabels(posiciones_1, :);
+posiciones_2 = find(trlabels_2 == 2);
+data_2 = tr(posiciones_2, :);
+labels_2 = trlabels(posiciones_2, :);
+posiciones_3 = find(trlabels_3 == 3);
+data_3 = tr(posiciones_3, :);
+labels_3 = trlabels(posiciones_3, :);
+
+posiciones_test = find(tslabels < 4);
+data_test = ts(posiciones_test, :);
+labels_test = tslabels(posiciones_test, :);
+
+#### CLASIFICADOR POR VOTACION DE 4 CLASES ####
+# 0 vs 1
+res_01 = svmtrain([labels_0, labels_1], [data_0, data_1], "");
+
+# 0 vs 2
+res_02 = svmtrain([labels_0, labels_2], [data_0, data_2], "");
+
+# 0 vs 3
+res_03 = svmtrain([labels_0, labels_3], [data_0, data_3], "");
+
+# 1 vs 2
+res_12 = svmtrain([labels_1, labels_2], [data_1, data_2], "");
+
+# 1 vs 2
+res_12 = svmtrain([labels_1, labels_2], [data_1, data_2], "");
+
+# 2 vs 3
+res_23 = svmtrain([labels_2, labels_3], [data_2, data_3], "");
+
+# predict
+[predicted_labels_01, accuracy, decision_values] = svmpredict(labels_test, data_test, res_01);
+[predicted_labels_02, accuracy, decision_values] = svmpredict(labels_test, data_test, res_02);
+[predicted_labels_03, accuracy, decision_values] = svmpredict(labels_test, data_test, res_03);
+[predicted_labels_12, accuracy, decision_values] = svmpredict(labels_test, data_test, res_12);
+[predicted_labels_13, accuracy, decision_values] = svmpredict(labels_test, data_test, res_13);
+[predicted_labels_23, accuracy, decision_values] = svmpredict(labels_test, data_test, res_23);
+
+
+
+
+####  CLASIFICADOR DAG DE 4 CLASES ####
+
+
+
+
+#### COMPARAR LOS MEJORES RESULTADOS OBTENIDOS ####
--- prac1.m
+++ prac1.m
@@ -1,60 +1,65 @@
 #!/usr/bin/octave
 
 graphics_toolkit(gnuplot);
-%inventado
+#inventado
 
 tr= load('data/mini/trSep.dat');
 trlabels = load('data/mini/trSeplabels.dat');
 
-% obtener SVM sin kernel
-res = svmtrain(trlabels, tr, '-t 0 -c 1'); %-t: type of SVM (0->linear), -c: cost
+################## EJERCICIO 3 ########################
+# obtener SVM sin kernel
+res = svmtrain(trlabels, tr, '-t 0 -c 1'); #-t: type of SVM (0->linear), -c: cost
 
-
-% multiplicadores de Lagrange
+# multiplicadores de Lagrange
 ml = res.sv_coef;
 
-
-% vectores soporte
+# vectores soporte
 vs = res.SVs;
 
-
-% vector de pesos
+# vector de pesos
 w = ml'*vs;
 
-
-% umbral de la funcion
+# umbral de la funcion
 w0 = -res.rho;
 
-
-% margen
+# margen
 margen = 1/(w*w');
 
-% calcular parametros de la frontera lineal
-
+# calcular parametros de la frontera lineal
 m = w(1)/w(2) ;
 b = w0/w(2);
 recta = -m - b;
-% representar graficamente
+# representar graficamente
 
 
 
 
 
 
-% no separable
+# no separable
 
 tr = load('data/mini/tr.dat');
 trlabels = load('data/mini/trlabels.dat');
-% obtener SVM sin kernel
-trres = svmtrain(trlabels, tr, '-t 0 -c 1'); %poner valor grande de c
-% multiplicadores de Lagrange
-mltr = trres.sv_coef;
-% vectores soporte
-vstr = trres.SVs;
-% vector de pesos
-wtr = mltr'*vstr;
-% umbral de la funcion
-w0tr = -trres.rho;
-% margen
-
+# obtener SVM sin kernel
+res = svmtrain(trlabels, tr, '-t 0 -c 1'); #poner valor grande de c
+# multiplicadores de Lagrange
+ml = res.sv_coef;
+# vectores soporte
+vs = res.SVs;
+# vector de pesos
+w = ml'*vs;
+# umbral de la funcion
+w0 = -res.rho;
+# margen
 margen = 1(w*w');
+# tolerancia del margen
+
+# representacion grafica, marcar vectores erroneos
+
+# graficas para diversos valores de C
+
+
+
+
+
+#################### EJERCICIO 4 ########################
