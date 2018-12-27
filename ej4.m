81cbdf563a5f6b5b8a897f639491a2a60a5b00be
NADAL ALMELA <silnaal1@EVIRL-021-OK.dsic.cloud>
2018-12-27 23:42:03 +0100

    esquema ej4 y ej5

--- ej4.m
+++ ej4.m
@@ -6,3 +6,22 @@ tr= load('data/spam/tr.dat');
 trlabels = load('data/spam/trlabels.dat');
 ts= load('data/spam/ts.dat');
 tslabels = load('data/spam/tslabels.dat');
+
+for C = [0.01, 0.1, 1, 10, 100]
+    
+    for kernel = 0:3
+
+        if kernel == 1
+            
+            for d = [2,3]
+                res = svmtrain();
+
+            end
+
+        else
+            res = svmtrain();
+        end
+
+    end
+
+end
--- ej5.m
+++ ej5.m
@@ -6,3 +6,24 @@ tr= load('data/usps/tr.dat');
 trlabels = load('data/usps/trlabels.dat');
 ts= load('data/usps/ts.dat');
 tslabels = load('data/usps/tslabels.dat');
+
+
+
+for C = [0.01, 0.1, 1, 10, 100]
+    
+    for kernel = 0:3
+
+        if kernel == 1
+            
+            for d = [2,3]
+                res = svmtrain();
+
+            end
+
+        else
+            res = svmtrain();
+        end
+
+    end
+
+end
