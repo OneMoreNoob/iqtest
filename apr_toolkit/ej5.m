051e3cfd25f306abc34fcfc614e3adc8a5403dc8
PUNJABI <akpun@EVIRL-025-OK.dsic.cloud>
2018-12-29 23:01:54 +0100

    Visualizacion de imagenes ej5

--- apr_toolkit/ej5.m
+++ apr_toolkit/ej5.m
@@ -10,7 +10,7 @@ load('data/usps/tslabels.dat');
 nElementsTest = rows(tslabels);
 
 
-for C = [0.01, 0.1, 1, 10, 100]
+for C = [0.01, 0.1, 1, 10, 100 ]
     C
     for kernel = 0:3
         kernel
@@ -23,6 +23,12 @@ for C = [0.01, 0.1, 1, 10, 100]
                 acc = accuracy(1) / 100
                 err = 1 - acc
                 confidence = 1.96*sqrt(acc*err/nElementsTest)
+				vectores = res.SVs;
+				for v = vectores'
+					x = reshape(v,16,16);
+					imshow(x')
+					pause();
+				endfor
             end
 
         else
@@ -31,6 +37,12 @@ for C = [0.01, 0.1, 1, 10, 100]
             acc = accuracy(1) / 100
             err = 1 - acc
             confidence = 1.96*sqrt(acc*err/nElementsTest)
+			vectores = res.SVs;
+				for v = vectores'
+					x = reshape(v,16,16);
+					imshow(x')
+					pause();
+				endfor
         end
         
 
